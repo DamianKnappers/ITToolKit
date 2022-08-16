4479f7f0d0195bed58b33dff7e07792184244b73
RH_Adv1c3 <cgknappers@gmail.com>
2022-08-16 12:56:32 +0200

    - Fixed TeamViewer detection, run and download ability - Fixed CCleaner detection, run and download ability

--- ITToolKit/Form1.Designer.cs
+++ ITToolKit/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.hd1 = new System.Windows.Forms.Label();
+            this.tvwButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.generalProgressbar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ccButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hd1
+            // 
+            this.hd1.AutoSize = true;
+            this.hd1.Location = new System.Drawing.Point(-4, 62);
+            this.hd1.Name = "hd1";
+            this.hd1.Size = new System.Drawing.Size(2647, 13);
+            this.hd1.TabIndex = 0;
+            this.hd1.Text = resources.GetString("hd1.Text");
+            // 
+            // tvwButton
+            // 
+            this.tvwButton.Location = new System.Drawing.Point(1189, 36);
+            this.tvwButton.Name = "tvwButton";
+            this.tvwButton.Size = new System.Drawing.Size(86, 23);
+            this.tvwButton.TabIndex = 1;
+            this.tvwButton.Text = "Installeer";
+            this.tvwButton.UseVisualStyleBackColor = true;
+            this.tvwButton.Click += new System.EventHandler(this.tvwButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1198, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TeamViewer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // generalProgressbar
+            // 
+            this.generalProgressbar.Location = new System.Drawing.Point(1114, 575);
+            this.generalProgressbar.Name = "generalProgressbar";
+            this.generalProgressbar.Size = new System.Drawing.Size(161, 23);
+            this.generalProgressbar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1111, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CCleaner";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ccButton
+            // 
+            this.ccButton.Location = new System.Drawing.Point(1097, 36);
+            this.ccButton.Name = "ccButton";
+            this.ccButton.Size = new System.Drawing.Size(86, 23);
+            this.ccButton.TabIndex = 4;
+            this.ccButton.Text = "Installeer";
+            this.ccButton.UseVisualStyleBackColor = true;
+            this.ccButton.Click += new System.EventHandler(this.ccButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1287, 610);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ccButton);
+            this.Controls.Add(this.generalProgressbar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tvwButton);
+            this.Controls.Add(this.hd1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label hd1;
+        private System.Windows.Forms.Button tvwButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar generalProgressbar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ccButton;
     }
 }
 
--- ITToolKit/Form1.cs
+++ ITToolKit/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,173 @@ namespace ITToolKit
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string twLocation;
+        public string twRunRunstring;
+        public string ccLocation;
+        public string ccRunRunString;
+        public string Username = Environment.UserName;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initiate the regedit searches //
+
+            // TeamViewer //
+            twLocation = this.SearchRegString(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "TeamViewer", "InstallLocation");
+            if (twLocation != string.Empty) {
+                tvwButton.Text = "Start";
+                twRunRunstring = this.SearchRegString(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "TeamViewer", "DisplayIcon");
+            } else {
+                twLocation = this.SearchRegString(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "TeamViewer", "InstallLocation");
+                twRunRunstring = this.SearchRegString(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "TeamViewer", "DisplayIcon");
+                if (twLocation != string.Empty)
+                {
+                    tvwButton.Text = "Start";
+                } else { tvwButton.Text = "Installeer"; }
+            }
+            // CCleaner //
+            ccLocation = this.SearchRegString(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "CCleaner", "InstallLocation");
+            if (ccLocation != string.Empty)
+            {
+                ccButton.Text = "Start";
+                ccRunRunString = this.SearchRegString(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "CCleaner", "DisplayIcon");
+            }
+            else
+            {
+                ccLocation = this.SearchRegString(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "CCleaner", "InstallLocation");
+                ccRunRunString = this.SearchRegString(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "CCleaner", "DisplayIcon");
+                if (ccLocation != string.Empty)
+                {
+                    ccButton.Text = "Start";
+                }
+                else { ccButton.Text = "Installeer"; }
+            }
+        }
+
+        private void ccButton_Click(object sender, EventArgs e)
+        {
+            if (ccButton.Text == "Start")
+            {
+                string runLocation = this.SearchRegString(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "CCleaner", "DisplayIcon");
+                if (ccRunRunString != null)
+                {
+                    System.Diagnostics.Process.Start(ccRunRunString);
+                }
+                else
+                {
+                    MessageBox.Show("Can\'t find the executable for this TeamViewer installation");
+                }
+            }
+            else
+            {
+                // CCleaner not installed, install code here
+                Uri dwUrl = new Uri("https://bits.avcdn.net/productfamily_CCLEANER/insttype_FREE/platform_WIN_PIR/installertype_ONLINE/build_RELEASE");
+                string filename = @"C:\Users\" + Username + @"\AppData\Local\Temp\ccleanerinstall.exe";
+                try
+                {
+                    if (File.Exists(filename))
+                    {
+                        File.Delete(filename);
+                    }
+                    else
+                    {
+                        WebClient wc = new WebClient();
+                        wc.DownloadFileAsync(dwUrl, filename);
+                        wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(tvwDownloadChanged);
+                        wc.DownloadFileCompleted += new AsyncCompletedEventHandler(ccDownloadCompleted);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void tvwButton_Click(object sender, EventArgs e)
+        {
+            if (tvwButton.Text == "Start") {
+                string runLocation = this.SearchRegString(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\", "DisplayName", "TeamViewer", "DisplayIcon");
+                if(twRunRunstring != null)
+                {
+                    System.Diagnostics.Process.Start(twRunRunstring);
+                } else
+                {
+                    MessageBox.Show("Can\'t find the executable for this TeamViewer installation");
+                }
+            } else {
+                // Teamviewer not installed, install code here
+                Uri dwUrl = new Uri("https://dl.teamviewer.com/download/version_15x/TeamViewer_Setup_x64.exe");
+                string filename = @"C:\Users\" + Username + @"\AppData\Local\Temp\tvwinstaller.exe";
+                try {
+                    if (File.Exists(filename))
+                    {
+                        File.Delete(filename);
+                    }
+                    else
+                    {
+                        WebClient wc = new WebClient();
+                        wc.DownloadFileAsync(dwUrl, filename);
+                        wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(tvwDownloadChanged);
+                        wc.DownloadFileCompleted += new AsyncCompletedEventHandler(tvwDownloadCompleted);
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+        
+        private void tvwDownloadChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            generalProgressbar.Value = e.ProgressPercentage;
+            if(generalProgressbar.Value == generalProgressbar.Maximum)
+            {
+                generalProgressbar.Value = 0;
+            }
+        }
+
+        private void ccDownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                MessageBox.Show("Download complete!, running exe", "Completed!");
+                System.Diagnostics.Process.Start(@"C:\Users\" + Username + @"\AppData\Local\Temp\ccleanerinstall.exe");
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong with the connection, please try again later");
+                MessageBox.Show(e.Error.ToString());
+            }
+        }
+
+        private void tvwDownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Error == null)
+            {
+                MessageBox.Show("Download complete!, running exe", "Completed!");
+                System.Diagnostics.Process.Start(@"C:\Users\"+ Username + @"\AppData\Local\Temp\tvwinstaller.exe");
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong with the connection, please try again later");
+                MessageBox.Show(e.Error.ToString());
+            }
+        }
+
+        private string SearchRegString(string keyname, string data, string valueToFind, string returnValue)
+        {
+            RegistryKey uninstallKey = Registry.LocalMachine.OpenSubKey(keyname);
+            var programs = uninstallKey.GetSubKeyNames();
+            foreach (var program in programs)
+            {  
+                RegistryKey subkey = uninstallKey.OpenSubKey(program);
+                if (string.Equals(valueToFind, subkey.GetValue(data, string.Empty).ToString(), StringComparison.CurrentCulture))
+                {
+                    return subkey.GetValue(returnValue).ToString();
+                }
+            }
+            return string.Empty;
         }
     }
 }
